fa402551f0d602b32585f0b38b0dbb549a99628c
xiaohua_global <xiaohua@qq.com>
2018-11-14 17:54:51 +0800

    第一个上传架构

---
+++ src/main/java/Text.java
@@ -0,0 +1,10 @@
+/**
+ * @version 1.0
+ * @Author Mr.zeng
+ * @Time 2018/11/14 17:53
+ */
+public class Text {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
